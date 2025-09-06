7fbdc131f39543a3953568a0d99f98a88439ad7f
ʟᴏɴɪꜱᴇᴅ <163004654+Lonised@users.noreply.github.com>
2025-09-06 12:03:43 +0500

    Add files via upload

---
+++ 06.09.25/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    // Изменение кодировки консоли (только Windows)
+    system("chcp 1251 > nul");
+
+    cout << "Программируем на C++" << endl;
+
+    system("pause > nul");
+    return 0;
+}
