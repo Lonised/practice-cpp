260231593e89ba665ba53ada32bb3c279365332c
ʟᴏɴɪꜱᴇᴅ <163004654+Lonised@users.noreply.github.com>
2025-10-06 14:18:56 +0500

    Update 1.cpp

--- 06.10.25/1.cpp
+++ 06.10.25/1.cpp
@@ -2,4 +2,25 @@
 using namespace std;
 
 int main() {
+  int Number;
+  cout << "Введите число maybe: ";
+  cin = Number;
+
+
+  if (Number <= 0 || Number == 0) {
+    cout << "Введите число больше нуля !";
+    return 0;
+  }
+
+  switch {
+    sase 1:
+      cout << "Вы человек-карьерист. Вам важна гонка в вашей жизни. Вы хотите быть везде и всегда первым." << endl;
+      break;
+    case 2:
+      cout << "Вы "
+  
+  }
+    
+  
+}
   
