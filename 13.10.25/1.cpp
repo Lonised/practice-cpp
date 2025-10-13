7fcec0ced51c2663878400e0b785001a11375aa4
ʟᴏɴɪꜱᴇᴅ <163004654+Lonised@users.noreply.github.com>
2025-10-13 14:20:55 +0500

    Create 1.cpp

---
+++ 13.10.25/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int = Number;
+  cout << "Введите число"; 
+  cin >> Number 
+
+  if (Number == 0 || Number <= -1) {
+    cout << "Введите положительное число, которое больше нуля !" << endl;
+  } else if (Number === int) {
+    cout << "Введите ЧИСЛО" << endl;
+  } 
+
+}
